7ce9dc636ef9d802b75c6f888e1a279b7e262fff
pruebasMLM <pruebasMLM@outlook.com>
2022-06-27 16:46:11 -0500

    Se suben los archivos base

---
+++ lib/src/features/search_page/search_page.dart
@@ -0,0 +1,16 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class seachPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+
+
+
+
